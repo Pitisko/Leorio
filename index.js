4a738630b8d0691dd3a7cb69427887d97d325157
tomu <66744640+Pitisko@users.noreply.github.com>
2020-11-21 21:58:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new Client({
 const config = require('./config.json')
 const prefix = config.prefix
 const token = config.token
+const db = require('quick.db')
 client.commands = new Collection();
 client.aliases = new Collection();
 client.categories = fs.readdirSync("./commands/");
@@ -25,6 +26,17 @@ client.on('ready', () => {
 })
 client.on('message', async message =>{
     if(message.author.bot) return;
+    if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
+        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
+        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
+        message.reply(`I removed your afk status! <:3260_leorioshine:778747536718168111> | Afk status - **${info}**`)
+    }
+    //checking for mentions
+    if(message.mentions.members.first()) {
+        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
+            message.channel.send(message.mentions.members.first().user.tag + ":" + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
+        }else return;
+    }else;
     if(!message.content.startsWith(prefix)) return;
     if(!message.guild) return;
     if(!message.member) message.member = await message.guild.fetchMember(message);
@@ -35,4 +47,75 @@ client.on('message', async message =>{
     if(!command) command = client.commands.get(client.aliases.get(cmd));
     if(command) command.run(client, message, args) 
 })
+
+const usersMap = new Map();
+const LIMIT = 5;
+const TIME = 7000;
+const DIFF = 3000;
+
+client.on('message', async(message) => {
+    if(message.author.bot) return;
+    if(usersMap.has(message.author.id)) {
+        const userData = usersMap.get(message.author.id);
+        const { lastMessage, timer } = userData;
+        const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+        let msgCount = userData.msgCount;
+        console.log(difference);
+
+        if(difference > DIFF) {
+            clearTimeout(timer);
+            console.log('Cleared Timeout');
+            userData.msgCount = 1;
+            userData.lastMessage = message;
+            userData.timer = setTimeout(() => {
+                usersMap.delete(message.author.id);
+                console.log('Removed from map.')
+            }, TIME);
+            usersMap.set(message.author.id, userData)
+        }
+        else {
+            ++msgCount;
+            if(parseInt(msgCount) === LIMIT) {
+                let muterole = message.guild.roles.cache.find(role => role.name === 'muted');
+                if(!muterole) {
+                    try{
+                        muterole = await message.guild.roles.create({
+                            name : "muted",
+                            permissions: []
+                        })
+                        message.guild.channels.cache.forEach(async (channel, id) => {
+                            await channel.createOverwrite(muterole, {
+                                SEND_MESSAGES: false,
+                                ADD_REACTIONS : false
+                            })
+                        })
+                    }catch (e) {
+                        console.log(e)
+                    }
+                }
+                message.member.roles.add(muterole);
+                message.channel.send('You have been muted!');
+                setTimeout(() => {
+                    message.member.roles.remove(muterole);
+                    message.channel.send('You have been unmuted!')
+                }, TIME);
+            } else {
+                userData.msgCount = msgCount;
+                usersMap.set(message.author.id, userData);
+            }
+        }
+    }
+    else {
+        let fn = setTimeout(() => {
+            usersMap.delete(message.author.id);
+            console.log('Removed from map.')
+        }, TIME);
+        usersMap.set(message.author.id, {
+            msgCount: 1,
+            lastMessage : message,
+            timer : fn
+        });
+    }
+})
+
 client.login(token)
